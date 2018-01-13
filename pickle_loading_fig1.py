bf9e3e1f9aa01b1f039077402dcee30adc76aa76
Maria J. Molina <hurricanemolina@gmail.com>
2018-01-13 02:49:48 -0500

    Create pickle_loading_fig1.py

---
+++ pickle_loading_fig1.py
@@ -0,0 +1,384 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 13 01:10:19 2018
+
+Maria J. Molina
+Ph.D. Student
+Central Michigan University
+
+"""
+
+
+import pickle
+
+
+
+
+with open('torday_neut1', 'wb') as output:
+    pickle.dump(sliced_gaussN, output, pickle.HIGHEST_PROTOCOL)
+
+with open('torday_nino1', 'wb') as output:
+    pickle.dump(sliced_gaussEN, output, pickle.HIGHEST_PROTOCOL)
+    
+with open('torday_nina1', 'wb') as output:
+    pickle.dump(sliced_gaussLN, output, pickle.HIGHEST_PROTOCOL)
+    
+with open('torday_low1', 'wb') as output:
+    pickle.dump(lo_thresh1, output, pickle.HIGHEST_PROTOCOL)
+    
+with open('torday_high1', 'wb') as output:
+    pickle.dump(hi_thresh1, output, pickle.HIGHEST_PROTOCOL)    
+    
+    
+    
+
+with open('torday_neut2', 'wb') as output:
+    pickle.dump(sliced_gaussN2, output, pickle.HIGHEST_PROTOCOL)
+
+with open('torday_nino2', 'wb') as output:
+    pickle.dump(sliced_gaussEN2, output, pickle.HIGHEST_PROTOCOL)
+    
+with open('torday_nina2', 'wb') as output:
+    pickle.dump(sliced_gaussLN2, output, pickle.HIGHEST_PROTOCOL)
+    
+with open('torday_low2', 'wb') as output:
+    pickle.dump(lo_thresh2, output, pickle.HIGHEST_PROTOCOL)
+    
+with open('torday_high2', 'wb') as output:
+    pickle.dump(hi_thresh2, output, pickle.HIGHEST_PROTOCOL) 
+    
+    
+    
+with open('torday_neut3', 'wb') as output:
+    pickle.dump(sliced_gaussN3, output, pickle.HIGHEST_PROTOCOL)
+
+with open('torday_nino3', 'wb') as output:
+    pickle.dump(sliced_gaussEN3, output, pickle.HIGHEST_PROTOCOL)
+    
+with open('torday_nina3', 'wb') as output:
+    pickle.dump(sliced_gaussLN3, output, pickle.HIGHEST_PROTOCOL)
+    
+with open('torday_low3', 'wb') as output:
+    pickle.dump(lo_thresh3, output, pickle.HIGHEST_PROTOCOL)
+    
+with open('torday_high3', 'wb') as output:
+    pickle.dump(hi_thresh3, output, pickle.HIGHEST_PROTOCOL) 
+    
+    
+
+with open('torday_neut4', 'wb') as output:
+    pickle.dump(sliced_gaussN4, output, pickle.HIGHEST_PROTOCOL)
+
+with open('torday_nino4', 'wb') as output:
+    pickle.dump(sliced_gaussEN4, output, pickle.HIGHEST_PROTOCOL)
+    
+with open('torday_nina4', 'wb') as output:
+    pickle.dump(sliced_gaussLN4, output, pickle.HIGHEST_PROTOCOL)
+    
+with open('torday_low4', 'wb') as output:
+    pickle.dump(lo_thresh4, output, pickle.HIGHEST_PROTOCOL)
+    
+with open('torday_high4', 'wb') as output:
+    pickle.dump(hi_thresh4, output, pickle.HIGHEST_PROTOCOL) 
+
+
+
+
+
+
+
+with open('tors_neut1', 'wb') as output:
+    pickle.dump(Gauss_SmoothTN1, output, pickle.HIGHEST_PROTOCOL)
+
+with open('tors_nino1', 'wb') as output:
+    pickle.dump(Gauss_SmoothTELN1, output, pickle.HIGHEST_PROTOCOL)
+    
+with open('tors_nina1', 'wb') as output:
+    pickle.dump(Gauss_SmoothTLN1, output, pickle.HIGHEST_PROTOCOL)
+    
+with open('tors_low1', 'wb') as output:
+    pickle.dump(lo_thresh1, output, pickle.HIGHEST_PROTOCOL)
+    
+with open('tors_high1', 'wb') as output:
+    pickle.dump(hi_thresh1, output, pickle.HIGHEST_PROTOCOL)    
+    
+    
+    
+
+with open('tors_neut2', 'wb') as output:
+    pickle.dump(Gauss_SmoothTN2, output, pickle.HIGHEST_PROTOCOL)
+
+with open('tors_nino2', 'wb') as output:
+    pickle.dump(Gauss_SmoothTELN2, output, pickle.HIGHEST_PROTOCOL)
+    
+with open('tors_nina2', 'wb') as output:
+    pickle.dump(Gauss_SmoothTLN2, output, pickle.HIGHEST_PROTOCOL)
+    
+with open('tors_low2', 'wb') as output:
+    pickle.dump(lo_thresh2, output, pickle.HIGHEST_PROTOCOL)
+    
+with open('tors_high2', 'wb') as output:
+    pickle.dump(hi_thresh2, output, pickle.HIGHEST_PROTOCOL) 
+    
+    
+    
+with open('tors_neut3', 'wb') as output:
+    pickle.dump(Gauss_SmoothTN3, output, pickle.HIGHEST_PROTOCOL)
+
+with open('tors_nino3', 'wb') as output:
+    pickle.dump(Gauss_SmoothTELN3, output, pickle.HIGHEST_PROTOCOL)
+    
+with open('tors_nina3', 'wb') as output:
+    pickle.dump(Gauss_SmoothTLN3, output, pickle.HIGHEST_PROTOCOL)
+    
+with open('tors_low3', 'wb') as output:
+    pickle.dump(lo_thresh3, output, pickle.HIGHEST_PROTOCOL)
+    
+with open('tors_high3', 'wb') as output:
+    pickle.dump(hi_thresh3, output, pickle.HIGHEST_PROTOCOL) 
+    
+    
+
+with open('tors_neut4', 'wb') as output:
+    pickle.dump(Gauss_SmoothTN4, output, pickle.HIGHEST_PROTOCOL)
+
+with open('tors_nino4', 'wb') as output:
+    pickle.dump(Gauss_SmoothTELN4, output, pickle.HIGHEST_PROTOCOL)
+    
+with open('tors_nina4', 'wb') as output:
+    pickle.dump(Gauss_SmoothTLN4, output, pickle.HIGHEST_PROTOCOL)
+    
+with open('tors_low4', 'wb') as output:
+    pickle.dump(lo_thresh4, output, pickle.HIGHEST_PROTOCOL)
+    
+with open('tors_high4', 'wb') as output:
+    pickle.dump(hi_thresh4, output, pickle.HIGHEST_PROTOCOL) 
+
+
+
+
+
+
+with open('stp_neut4', 'wb') as output:
+    pickle.dump(sliced_gaussN, output, pickle.HIGHEST_PROTOCOL)
+
+with open('stp_nino4', 'wb') as output:
+    pickle.dump(sliced_gaussEN, output, pickle.HIGHEST_PROTOCOL)
+    
+with open('stp_nina4', 'wb') as output:
+    pickle.dump(sliced_gaussLN, output, pickle.HIGHEST_PROTOCOL)
+    
+with open('stp_low4', 'wb') as output:
+    pickle.dump(lo_thresh1, output, pickle.HIGHEST_PROTOCOL)
+    
+with open('stp_high4', 'wb') as output:
+    pickle.dump(hi_thresh1, output, pickle.HIGHEST_PROTOCOL) 
+    
+    
+
+
+    
+with open('stpse_neut4', 'wb') as output:
+    pickle.dump(sliced_gaussN, output, pickle.HIGHEST_PROTOCOL)
+
+with open('stpse_nino4', 'wb') as output:
+    pickle.dump(sliced_gaussEN, output, pickle.HIGHEST_PROTOCOL)
+    
+with open('stpse_nina4', 'wb') as output:
+    pickle.dump(sliced_gaussLN, output, pickle.HIGHEST_PROTOCOL)
+    
+with open('stpse_low4', 'wb') as output:
+    pickle.dump(lo_thresh1, output, pickle.HIGHEST_PROTOCOL)
+    
+with open('stpse_high4', 'wb') as output:
+    pickle.dump(hi_thresh1, output, pickle.HIGHEST_PROTOCOL) 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+import pickle
+
+
+with open('torday_neut1', 'rb') as f:
+     torday_neut1 = pickle.load(f)
+
+with open('torday_nino1', 'rb') as f:
+     torday_nino1 = pickle.load(f)
+    
+with open('torday_nina1', 'rb') as f:
+     torday_nina1 = pickle.load(f)
+    
+with open('torday_low1', 'rb') as f:
+     torday_low1 = pickle.load(f)
+    
+with open('torday_high1', 'rb') as f:
+     torday_high1 = pickle.load(f)    
+    
+    
+    
+
+with open('torday_neut2', 'rb') as f:
+    torday_neut2 = pickle.load(f)
+
+with open('torday_nino2', 'rb') as f:
+    torday_nino2 = pickle.load(f)
+    
+with open('torday_nina2', 'rb') as f:
+    torday_nina2 = pickle.load(f)
+    
+with open('torday_low2', 'rb') as f:
+    torday_low2 = pickle.load(f)
+    
+with open('torday_high2', 'rb') as f:
+    torday_high2 = pickle.load(f) 
+    
+    
+    
+with open('torday_neut3', 'rb') as f:
+    torday_neut3 = pickle.load(f)
+
+with open('torday_nino3', 'rb') as f:
+    torday_nino3 = pickle.load(f)
+    
+with open('torday_nina3', 'rb') as f:
+    torday_nina3 = pickle.load(f)
+    
+with open('torday_low3', 'rb') as f:
+    torday_low3 = pickle.load(f)
+    
+with open('torday_high3', 'rb') as f:
+    torday_high3 = pickle.load(f)
+    
+    
+
+with open('torday_neut4', 'rb') as f:
+    torday_neut4 = pickle.load(f)
+
+with open('torday_nino4', 'rb') as f:
+    torday_nino4 = pickle.load(f)
+    
+with open('torday_nina4', 'rb') as f:
+    torday_nina4 = pickle.load(f)
+    
+with open('torday_low4', 'rb') as f:
+    torday_low4 = pickle.load(f)
+    
+with open('torday_high4', 'rb') as f:
+    torday_high4 = pickle.load(f)
+
+
+
+with open('tors_neut1', 'rb') as f:
+    tors_neut1 = pickle.load(f)
+
+with open('tors_nino1', 'rb') as f:
+    tors_nino1 = pickle.load(f)
+    
+with open('tors_nina1', 'rb') as f:
+    tors_nina1 = pickle.load(f)
+    
+with open('tors_low1', 'rb') as f:
+    tors_low1 = pickle.load(f)
+    
+with open('tors_high1', 'rb') as f:
+    tors_high1 = pickle.load(f) 
+    
+    
+    
+
+with open('tors_neut2', 'rb') as f:
+    tors_neut2 = pickle.load(f)
+
+with open('tors_nino2', 'rb') as f:
+    tors_nino2 = pickle.load(f)
+    
+with open('tors_nina2', 'rb') as f:
+    tors_nina2 = pickle.load(f)
+    
+with open('tors_low2', 'rb') as f:
+    tors_low2 = pickle.load(f)
+    
+with open('tors_high2', 'rb') as f:
+    tors_high2 = pickle.load(f)
+    
+    
+    
+with open('tors_neut3', 'rb') as f:
+    tors_neut3 = pickle.load(f)
+
+with open('tors_nino3', 'rb') as f:
+    tors_nino3 = pickle.load(f)
+    
+with open('tors_nina3', 'rb') as f:
+    tors_nina3 = pickle.load(f)
+    
+with open('tors_low3', 'rb') as f:
+    tors_low3 = pickle.load(f)
+    
+with open('tors_high3', 'rb') as f:
+    tors_high3 = pickle.load(f)
+    
+    
+
+with open('tors_neut4', 'rb') as f:
+    tors_neut4 = pickle.load(f)
+
+with open('tors_nino4', 'rb') as f:
+    tors_nino4 = pickle.load(f)
+    
+with open('tors_nina4', 'rb') as f:
+    tors_nina4 = pickle.load(f)
+    
+with open('tors_low4', 'rb') as f:
+    tors_low4 = pickle.load(f)
+    
+with open('tors_high4', 'rb') as f:
+    tors_high4 = pickle.load(f)
+
+
+
+
+
+with open('stp_neut4', 'rb') as f:
+    stp_neut4 = pickle.load(f)
+
+with open('stp_nino4', 'rb') as f:
+    stp_nino4 = pickle.load(f)
+    
+with open('stp_nina4', 'rb') as f:
+    stp_nina4 = pickle.load(f)
+    
+with open('stp_low4', 'rb') as f:
+    stp_low4 = pickle.load(f)
+    
+with open('stp_high4', 'rb') as f:
+    stp_high4 = pickle.load(f)
+    
+    
+    
+with open('stpse_neut4', 'rb') as f:
+    stpse_neut4 = pickle.load(f)
+
+with open('stpse_nino4', 'rb') as f:
+    stpse_nino4 = pickle.load(f)
+    
+with open('stpse_nina4', 'rb') as f:
+    stpse_nina4 = pickle.load(f)
+    
+with open('stpse_low4', 'rb') as f:
+    stpse_low4 = pickle.load(f)
+    
+with open('stpse_high4', 'rb') as f:
+    stpse_high4 = pickle.load(f)
+    
+    
+    
+    
